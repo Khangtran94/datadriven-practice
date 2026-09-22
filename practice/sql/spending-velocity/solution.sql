7078eb474d7eeb0b4820c01734d07d437bb67c1a
Khang Tran <tranhoangngockhang1994@gmail.com>
2026-09-22 11:58:54 +0700

    Solve spending-velocity

---
+++ practice/sql/spending-velocity/solution.sql
@@ -0,0 +1,3 @@
+SELECT user_id, transaction_date, total_amount,
+      SUM(total_amount) OVER(PARTITION BY user_id ORDER BY transaction_date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS rolling_sum
+FROM transactions
