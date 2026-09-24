546f953c20b6aac1a4f48a40fc2f3b89db1b67f8
Khang Tran <tranhoangngockhang1994@gmail.com>
2026-09-24 23:03:11 +0700

    Solve the-order-enforcer

--- practice/python/the-order-enforcer/solution.py
+++ practice/python/the-order-enforcer/solution.py
@@ -1,6 +1,8 @@
 def validate_ab_string(s: str) -> bool:
-  for i,c in enumerate(s):
-    if i+1 < len(s):
-      if s[i+1] == 'a' and s[i] == 'b':
-        return False
+  see_b = False
+  for c in s:
+    if c == 'b':
+      see_b = True
+    elif c == 'a' and see_b:
+      return False
   return True
